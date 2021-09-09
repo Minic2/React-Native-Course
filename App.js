a19326740b8d0ea06620f730c58c6adaa0d50923
Minic2 <you@example.com>
2021-09-08 23:32:22 -0300

    first react-native

--- App.js
+++ App.js
@@ -1,21 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  Button,
+  Alert,
+  TouchableOpacity,
+  Platform
+} from "react-native";
+import image from "./assets/diamond-red.png";
+import * as ImagePicker from "expo-image-picker";
+import * as Sharing from "expo-sharing";
+const App = () => {
+  const [selectedImage, setselectedImage] = useState(null);
 
-export default function App() {
+  let openImagePickerAsync = async () => {
+    let permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (permissionResult.granted === false) {
+      alert("Permision to acces galery is required");
+      return;
+    }
+    const pickerResult = await ImagePicker.launchImageLibraryAsync();
+
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+    setselectedImage({ localUri: pickerResult.uri });
+  };
+
+  const openShareDialog = async () => {
+    if (!(await Sharing.isAvailableAsync())) {
+      alert("la opcion compartir no esta disponible en su plataforma");
+      return;
+    }
+    await Sharing.shareAsync(selectedImage.localUri);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Selecciona un Avatar</Text>
+      <TouchableOpacity onPress={openImagePickerAsync}>
+        <Image
+          source={{
+            uri:
+              selectedImage !== null
+                ? selectedImage.localUri
+                : "https://image.flaticon.com/icons/png/512/5019/5019638.png",
+          }}
+          style={styles.image}
+        />
+      </TouchableOpacity>
+
+      {/* <Button
+      color="#000"
+      title="Press me"
+      onPress={()=> Alert.alert("hello")}
+/> */}
+      {selectedImage ? (
+        <TouchableOpacity onPress={openShareDialog} style={styles.button}>
+          <Text style={styles.buttonText}>Compartir</Text>
+        </TouchableOpacity>
+      ) : (
+        <View />
+      )}
     </View>
   );
-}
-
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#221f1f",
+  },
+  title: {
+    fontSize: 30,
+    color: "#f5f5f1",
+  },
+  image: {
+    height: 180,
+    width: 180,
+    resizeMode: "contain",
+    borderRadius: 90,
+  },
+  button: {
+    backgroundColor: "#e50914",
+    padding: 7,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: "#f5f5f1",
+    fontSize: 20,
   },
 });
+
+export default App;
